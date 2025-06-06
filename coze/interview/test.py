2a130e858c81cc79dcd3c5c109ea8a32b12cd39b
xr <192319969@qq.com>
2025-06-07 00:31:19 +0800

    style

--- coze/interview/test.py
+++
@@ -1,7 +0,0 @@
-from recorder.xfyun_asr import ASRClient
-
-asr = ASRClient(app_id="49fade5c", api_key="03887408f57f7f23fa36fd17371cb3ff", api_secret="YmM1MTFlZjMwYmRmMDQ5MGM0ZmE4Yzcy")
-asr.start()
-
-# 如果你想获取识别后的文本（可在主线程等待一段时间后再调用）
-print(asr.get_result())
